81881980952ee44ab88e0dbb5e17a79820828e53
dao51 <fsd5816@163.com>
2021-01-19 15:08:38 +0800

    useRef常用方式

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,20 +1,47 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import useMousePosition from '../hooks/useMousePosition'
 
 const LikeButton: React.FC = () => {
     const [like, setLike] = useState(0) // 使用useState设置初始值，相当于state中设置默认值
     const [on, setOn] = useState(true)
-    const positions = useMousePosition()// 调用自定义hook
+    // const positions = useMousePosition()// 调用自定义hook
+    const likeRef = useRef(0)  // 设置useRef初始值
+    const didMountRef = useRef(false)
+    const domRef = useRef<HTMLInputElement>(null)
+
     useEffect(() => { // useEffect 在首次及之后每次渲染后自动调用
         document.title = `点击了${like}次`
+    }, [like])
+
+    useEffect(() => {
+        if(didMountRef.current) {
+            console.log('非首次加载')
+        } else {
+            didMountRef.current = true
+            console.log('首次加载')
+        }
     })
+
+    useEffect(() => {
+        if(domRef && domRef.current) {
+            domRef.current.focus()
+        }
+    })
+
+    function handleAlertClick() {
+        setTimeout(() => {
+            alert('你点击了' + likeRef.current)
+        }, 3000)
+    }
 
     return (
         <>
-            <h2>x:{positions.x},y:{positions.y}</h2>
-            <button onClick={() => {setLike(like + 1)}}>
+            <input type="text" ref={domRef} />
+            {/* <h2>x:{positions.x},y:{positions.y}</h2> */}
+            <button onClick={() => {setLike(like + 1); likeRef.current++}}>
                 {like}👍
             </button>
+            <button onClick={handleAlertClick}>Alert!</button>
             <button onClick={() => {setOn(!on)}}>
                 { on ? 'ON' : 'OFF' }
             </button>
